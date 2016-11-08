d6be5f53da635214ba318b572324d81f287067a7
Kostis Anagnostopoulos <ankostis@gmail.com>
2016-11-08 19:42:01 +0100

    fix(tkui): add icons in setup.py's package-data

--- setup.py
+++ setup.py
@@ -190,7 +190,7 @@ setup(
     packages=find_packages(exclude=["tests"]),
     #include_package_data = True,
     package_data={
-        proj_name: ['excel/*.vba', 'excel/*.ico'],
+        proj_name: ['excel/*.vba', 'excel/*.ico', 'icons/*'],
     },
     install_requires=install_requires,
     setup_requires=[
