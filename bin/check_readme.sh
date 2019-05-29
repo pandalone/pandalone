76e4855a1772ff1cb6e8c957b9674edb52122e25
Kostis Anagnostopoulos <ankostis@gmail.com>
2019-05-29 14:17:50 +0200

    enh(build): use better `twine` to check README for PyPi

--- bin/check_readme.sh
+++ bin/check_readme.sh
@@ -1,42 +1,34 @@
 #!/bin/bash
 #-*- coding: utf-8 -*-
 #
-# Copyright 2013-2015 European Commission (JRC);
+# Copyright 2013-2019 European Commission (JRC);
 # Licensed under the EUPL (the 'Licence');
 # You may not use this work except in compliance with the Licence.
 # You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
 
 
 ## Checks that README has no RsT-syntactic errors.
-# Since it is used by `setup.py`'s `description` if it has any errors,
+# Since it is used by `setup.py`'s `description` if it has any errors, 
 # PyPi would fail parsing them, ending up with an ugly landing page,
 # when uploaded.
 
-set +x ## Enable for debug
+set +x  # Set -x for debugging script.
 
 my_dir=`dirname "$0"`
-cd $my_dir/..
-
-py=""
-rst="rst2html"
-if [ ! -x "`which $rst 2>/dev/null`" ]; then
-    ## In WinPython, only a python-script exist in PATH,
-    #   so execute it with python-interpreter.
-    #
-    exe="`which rst2html.py 2> /dev/null`"
-    if [ $? -eq 0 ]; then
-        py=python
-        rst="$exe"
-    else
-        echo -e "Cannot find 'rst2html'! \n Sphinx installed? `pip show sphinx`" &&
-        exit 1
-    fi
-
-    if [ -x "`which cygpath`" ]; then
-        rst="`cygpath -w $rst`"
-    fi
+my_name=`basename "$0"`
+my_dir="$(dirname "$0")"
+if [ -f "$my_dir/../setup.py" ]; then
+        cd "$my_dir/.."
+else
+        my_dir="$PWD"
 fi
 
-export PYTHONPATH='$my_dir/..'
-#python setup.py --long-description > t.rst
-python setup.py --long-description | $py "$rst"  --halt=warning > /dev/null && echo OK
+tmp_dir=$(mktemp -d -t wltp-$my_name-XXXXXXXXXX)
+
+rm -rf "build/*" "dist/*"
+python setup.py -q bdist_wheel --dist-dir $tmp_dir && \
+twine check $tmp_dir/*.whl 
+
+exc=$?
+rm -rf $tmp_dir
+exit $exc
