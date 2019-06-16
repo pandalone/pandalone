0a08792ffb20a0fd27f49cc623ed4941ad782411
Kostis Anagnostopoulos <ankostis@gmail.com>
2019-06-16 11:46:28 +0200

    chore(ver): 0.3.0.dev1 --> .dev2

--- pandalone/_version.py
+++ pandalone/_version.py
@@ -5,6 +5,6 @@
 # You may not use this work except in compliance with the Licence.
 # You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
 """Authoritative project's version-info"""
-__version__ = "0.3.0.dev1"  # PEP 440 versioning. Also update README.rst's.
-__updated__ = "2019-06-16 01:33:25"
+__version__ = "0.3.0.dev2"  # PEP 440 versioning. Also update README.rst's.
+__updated__ = "2019-06-16 11:45:30"
 version = __version__
