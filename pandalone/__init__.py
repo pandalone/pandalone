7ded91a5faac83a3671caa05d5edf765763b5a5e
Kostis Anagnostopoulos <ankostis@gmail.com>
2020-04-05 23:31:41 +0300

    doc: pep8 project coorda after imports

--- pandalone/__init__.py
+++ pandalone/__init__.py
@@ -1,10 +1,10 @@
-from ._version import __version__, __updated__
+"""Collection of utilities for working with hierarchical data with relocatable paths."""
 
 __copyright__ = "Copyright (C) 2013-2015 European Commission (JRC)"
 __license__ = "EUPL 1.1+"
 
 __title__ = "pandalone"
-__summary__ = (
-    "Collection of utilities for working with hierarchical data with relocatable paths."
-)
+__summary__ = __doc__.splitlines()[0]
 __uri__ = "https://github.com/pandalone/pandalone"
+
+from ._version import __version__, __updated__
